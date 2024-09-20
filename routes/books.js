d472ffdb36161775570ca50ea2c744734cd388a3
Isadorq <iscapatice@gmail.com>
2024-09-19 22:45:34 -0300

    att

---
+++ routes/books.js
@@ -0,0 +1,36 @@
+const express = require('express')  // Importamos o Express
+const Book = require('../models/Book')  // Importamos o modelo Book
+const router = express.Router() // Criamos o roteador
+
+// ** CRIAÇÃO (POST) **
+router.post('/', async (req, res) => {
+    const { title, author, year } = req.body;   // Extraímos os dados da requisição
+    try {
+        const newBook = ({ title, author, year })  // Criamos e salvamos o livro
+        await newBook.save();
+        res.status(201).json(newBook);  // Retornamos o livro criado
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao criar livro'})
+    }
+})
+
+// ** LEITURA (PUT) **
+router.get('/', async (req, res) => {
+    try{
+        const books = await Book.find() // Buscamos todos os livros
+        res.status(200).json(books)
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao buscar livros', error })   // Retornamos erro, se houver
+    }
+})
+
+// *** ATUALIZAÇÃO (PUT) ***
+router.put('/:id', async (req, res) => {
+    const { title, author, year} = req.body // Extraímos os novos dados
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(req.parans.id, { title, author, year}, (new true))    // Atualizamos o livro pelo ID
+        res.status(500).json(updatedBook)   // Retornamos o livro atualizado
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao atualizar livro'})
+    }
+})
