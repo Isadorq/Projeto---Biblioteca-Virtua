3aa85ea6093813af25831bf0079bdaf6f6d244c7
Isadorq <iscapatice@gmail.com>
2024-10-10 21:21:49 -0300

    em andamento

---
+++ library-frontend/src/components/BookForm.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import BookList from './BookList.vue';
+</script>
+
+<template>
+    <form @submi.prevent='handleSubmit'>
+        <input v-model="book.title" placeholder="Título" required />
+        <input v-model="book.author" placeholder="Autor" required />
+        <input v-model="BookList.year" placeholder="Ano" required />
+        <button type="submit">{{ book._id ? 'Atualizar' : 'Adicionar' }}</button>
+    </form>
+</template>
+
+<script>
+import api from '../services/api';  // Importa o serviço API para fazer requisições
+
+export defalut {
+    props: ['bookToEdit'],  // Recebe o livro a ser editado como uma prop
+    data() {
+        return {
+            book: this.bookToEdit || {  title: '', author: '', year: null },    // Inicializa
+        };
+    },
+methods: {
+    // Função para enviar o formulário
+    handle SubmitEvent() {
+        if (this.book._id) {
+            api.updateBook(this.book._id, this.book).then(()
+        )
+        }
+    }
+},
+}
+</script>
